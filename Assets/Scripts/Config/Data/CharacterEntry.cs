3d25a03a907c25a90bacff0338e6b878a99ffae2
Yaroslav Skripka <50421347+YaroslavSkrip1@users.noreply.github.com>
2025-10-27 19:33:13 +0200

    Setup

---
+++ Assets/Scripts/Config/Data/CharacterEntry.cs
@@ -0,0 +1,12 @@
+using System;
+using UnityEngine;
+
+namespace DefaultNamespace;
+
+[Serializable]
+public class CharacterEntry
+{
+    public string Id;
+    public GameObject PreviewPrefab;
+    public GameObject RuntimePrefab; 
+}
